69bba8ceb48528cc4d710036df662d5afe15c4f2
Guillaume Hitier <guillaume.hitier@epitech.eu>
2016-05-01 15:28:43 +0200

    generic functions added

--- functions.lua
+++ functions.lua
@@ -1,73 +1,68 @@
 require 'cairo'
 
-function draw_cpu(cr)
+function draw_cpu(cr, cores)
 
-	local mult = 4
 	local x, y = 70, 230
-	local w, h = (100 * mult), 4
-	local cpu0 = conky_parse("${cpu cpu0}")
-	local cpu1 = conky_parse("${cpu cpu1}")
-	local cpu2 = conky_parse("${cpu cpu2}")
-	local cpu3 = conky_parse("${cpu cpu3}")
-	local cpu4 = conky_parse("${cpu cpu4}")
-	cairo_set_source_rgba (cr,1,1,1,1)
-	cairo_rectangle (cr, x + w, y, -(cpu1 * mult), h)
-	cairo_rectangle (cr, x + w, y + (h + 1), -(cpu2 * mult), h)
-	cairo_rectangle (cr, x + w, y + (h + 1) * 2, -(cpu3 * mult), h)
-	cairo_rectangle (cr, x + w, y + (h + 1) * 3, -(cpu4 * mult), h)
-	cairo_fill (cr)
-	cairo_set_source_rgba (cr,1,1,1,1)
-	cairo_select_font_face (cr, "Unispace", CAIRO_FONT_SLANT_NORMAL,
-                               CAIRO_FONT_WEIGHT_NORMAL)
-	cairo_set_font_size (cr, 22.0)
-	cairo_set_source_rgba (cr, hex_to_rgb(0x0C2E8A, 1))
-	cairo_move_to (cr, x + w + 35, y + (h + 1) * 4)
-	cairo_show_text (cr, "CPU ")
-	cairo_show_text(cr, string.format("%02d", cpu0))
-	cairo_show_text(cr, "%")
-	cairo_stroke (cr)
+	local w, h = 400, 20
+	local cpu = conky_parse("${cpu cpu0}")
+	local text = "CPU "..string.format("%02d", cpu).."%"
+
+	if cores == 0 then
+		draw_bar(cr, x, y, w, h, cpu, color1)
+	else
+		for i = 1, cores, 1 do
+			cpu = conky_parse("${cpu cpu"..string.format("%d", i).."}")
+			draw_bar(cr, x, y + (h / cores) * (i - 1),
+						w, (h / cores) - 1, cpu, color1)
+		end
+	end
+	draw_text(cr, x + w + 38, y + h, text,
+				"Unispace", 22.0, color2)
+	mult, x, y, w, h, cpu, text = nil
 end
 
 function draw_ram(cr)
 
-	local mult = 4
 	local x, y = 70, 190
-	local w, h = (100 * mult), 20
+	local w, h = 400, 20
 	local ram = conky_parse("$memperc")
-	cairo_set_source_rgba (cr,1,1,1,1)
-	cairo_rectangle (cr, x + w, y, -(ram * mult), h)
-	cairo_fill (cr)
-	cairo_set_source_rgba (cr,1,1,1,1)
-	cairo_select_font_face (cr, "Unispace", CAIRO_FONT_SLANT_NORMAL,
-                               CAIRO_FONT_WEIGHT_NORMAL)
-	cairo_set_font_size (cr, 22.0)
-	cairo_set_source_rgba (cr, hex_to_rgb(0x0C2E8A, 1))
-	cairo_move_to (cr, x + w + 35, y + h)
-	cairo_show_text (cr, "RAM ")
-	cairo_show_text(cr, string.format("%02d", ram))
-	cairo_show_text(cr, "%")
-	cairo_stroke (cr)
+	local text = "RAM "..string.format("%02d", ram).."%"
+	
+	draw_bar(cr, x, y, w, h, ram, color1)
+	draw_text(cr, x + w + 38, y + h, text,
+				"Unispace", 22.0, color2)
+	mult, x, y, w, h, ram, text = nil
 end
 
 function draw_disk(cr)
 
-	local mult = 4
 	local x, y = 70, 150
-	local w, h = (100 * mult), 20
+	local w, h = 400, 20
 	local disk = conky_parse("${fs_used_perc /}")
-	cairo_set_source_rgba (cr,1,1,1,1)
-	cairo_rectangle (cr, x + w, y, -(disk * mult), h)
-	cairo_fill (cr)
-	cairo_set_source_rgba (cr,1,1,1,1)
-	cairo_select_font_face (cr, "Unispace", CAIRO_FONT_SLANT_NORMAL,
+	local text = "DISK "..string.format("%02d", disk).."%"
+
+	draw_bar(cr, x, y, w, h, disk, color1)
+	draw_text(cr, x + w + 25, y + h, text,
+				"Unispace", 22.0, color2)
+	mult, x, y, w, h, disk, text = nil
+end
+
+function draw_bar(cr, x, y, w, h, perc, color)
+	cairo_set_source_rgba(cr,hex_to_rgb(color, 1))
+	cairo_rectangle(cr, x + w, y, -(perc * (w / 100)), h)
+	cairo_fill(cr)
+	cairo_set_source_rgba(cr, 1, 1, 1, 1)
+end
+
+function draw_text(cr, x, y, text, font, size, color)
+	cairo_select_font_face (cr, font, CAIRO_FONT_SLANT_NORMAL,
                                CAIRO_FONT_WEIGHT_NORMAL)
-	cairo_set_font_size (cr, 22.0)
-	cairo_set_source_rgba (cr, hex_to_rgb(0x0C2E8A, 1))
-	cairo_move_to (cr, x + w + 25, y + h)
-	cairo_show_text (cr, "DISK ")
-	cairo_show_text(cr, string.format("%02d", disk))
-	cairo_show_text(cr, "%")
-	cairo_stroke (cr)
+	cairo_set_font_size(cr, size)
+	cairo_set_source_rgba(cr, hex_to_rgb(color, 1))
+	cairo_move_to(cr, x, y)
+	cairo_show_text(cr, text)
+	cairo_stroke(cr)
+	cairo_set_source_rgba(cr, 1, 1, 1, 1)
 end
 
 function hex_to_rgb(colour, alpha)
@@ -88,11 +83,15 @@ function conky_main()
     local cr = cairo_create(cs)
     local updates = tonumber(conky_parse('${updates}'))
     
+    color1 = 0xFFFFFF
+    color2 = 0x0C2E8A
+    cpu_cores = 4 -- Number of CPU cores (0 for sum of all CPU cores)
+
     if updates > 5
     	then
     		draw_disk(cr)
     		draw_ram(cr)
-    		draw_cpu(cr)
+    		draw_cpu(cr, cpu_cores)
     end
 
     cairo_surface_destroy(cs)
